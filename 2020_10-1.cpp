cd8a450b982f4c10e9ace373cc3fab510b50ed4f
Aura <63115437+aura01221@users.noreply.github.com>
2021-10-24 16:02:51 +0800

    Update 2020_10-1.cpp

--- 2020_10-1.cpp
+++ 2020_10-1.cpp
@@ -3,7 +3,9 @@ using namespace std;
 int main()
 {
     int a1, b1, c1, a2, b2, c2, n, x1, x2, y1, y2, ans = -1000000;
+    //輸入
     cin >> a1 >> b1 >> c1 >> a2 >> b2 >> c2 >> n;
+    //執行n次
     for (x1 = 0; x1 <= n; x1++)
     {
         x2 = n - x1;
@@ -12,5 +14,6 @@ int main()
         if (ans < y1 + y2)
             ans = y1 + y2;
     }
+    //輸出結果
     cout << ans << endl;
 }
